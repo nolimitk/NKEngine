75dc28cfbd99960223da631060e9e94fecca0c8c
Jon Kim <kizz@naver.com>
2016-05-23 23:42:36 +0900

    memory delete array

--- NKEngine/NKCore/NKMemory.h
+++ NKEngine/NKCore/NKMemory.h
@@ -7,9 +7,13 @@
 
 #include "../../external/vld/inc/vld.h"
 
+// @todo engine configuration
+#define __TURNOFF_MEMORYPOOL__
+
 void* operator new(size_t size);
 void operator delete(void *p);
 
 #define SAFE_DELETE(p) { if(p){ delete p; p=nullptr; } }
+#define SAFE_DELETE_ARRAY(p) { if(p){ delete[] p; p=nullptr; } }
 
 #endif // __NKMEMORY_HEADER__
