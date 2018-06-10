31191dbc1fdda25bfe684245ac04625ee4a5ef7c
Jon Kim <kizz@naver.com>
2018-06-10 23:14:32 +0900

    [NKUnittest] job slot unittest

--- NKEngineUnitTest/NKSchedulerTest.cpp
+++ NKEngineUnitTest/NKSchedulerTest.cpp
@@ -1,7 +1,3 @@
-#pragma once
-
-#ifndef __NKSCHEDULERTEST_HEADER__
-#define __NKSCHEDULERTEST_HEADER__
 // @nolimitk
 // 16.05.28
 // scheduler test
@@ -12,26 +8,44 @@
 
 using namespace std;
 
+/*
 NKTEST(EventSlot_Test)
 {
 	_ASSERT(NKNetwork::IOCPManager::getInstance()->create() == true);
 
-	class MockSlot : public NKScheduler::EventSlot
+	class MockSlot : public NKScheduler::JobSlot
 	{
+	public:
+		bool _onProcess;
+
 		virtual bool onProcess(NKNetwork::EventContext& event_context, uint32_t transferred) override
 		{
-			NKUNITTESTLOG_INFO(L"%s", __FUNCTIONW__);
+			_onProcess = true;
 			return true;
 		}
+
+		MockSlot(void) :JobSlot(10),_onProcess(false) {}
 	};
 
 	shared_ptr<MockSlot> test_event = make_shared<MockSlot>();
 
 	_ASSERT(NKNetwork::IOCPManager::getInstance()->postEvent(dynamic_pointer_cast<NKNetwork::EventObject>(test_event), 0) == true);
+	WAITFOR(test_event, onProcess);
 
 	NKNetwork::IOCPManager::getInstance()->close();
 
 	return true;
 }
+*/
 
-#endif // __NKSCHEDULERTEST_HEADER__
+NKTEST(Scheduler_Test)
+{
+	_ASSERT(NKNetwork::IOCPManager::getInstance()->create() == true);
+	_ASSERT(NKScheduler::Scheduler::getInstance()->start() == true);
+	_ASSERT(NKScheduler::Scheduler::getInstance()->stop() == true);
+
+	NKScheduler::Scheduler::getInstance()->destroy();
+	NKNetwork::IOCPManager::getInstance()->close();
+
+	return true;
+}
