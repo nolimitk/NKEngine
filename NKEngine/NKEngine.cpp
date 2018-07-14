fb70f2f352de887892584a895dbe640a291b6ad4
Jon Kim <kizz@naver.com>
2018-07-15 00:06:06 +0900

    [NKEngine] engine initialization

--- NKEngine/NKEngine.cpp
+++ NKEngine/NKEngine.cpp
@@ -3,12 +3,29 @@
 
 bool NKEngine::StartEngine(void)
 {
+	setlocale(LC_ALL, "korean");
+
+	if (NKNetwork::IOCPManager::getInstance()->create() == false)
+	{
+		// @TODO 이렇게 되면 error를 두번 찍고 assert도 두번 걸린다. exception으로 처리하도록 변경하자
+		NKENGINELOG_ERROR_ASSERT(L"NKNetwork failed to start.");
+		return false;
+	}
+
+	if (NKScheduler::Scheduler::getInstance()->start() == false)
+	{
+		return false;
+	}
+
 	return true;
 }
 
 void NKEngine::ReleaseEngine(void)
 {
+	NKScheduler::Scheduler::getInstance()->stop();
+	NKNetwork::IOCPManager::getInstance()->close();
+
+	NKScheduler::Scheduler::getInstance()->destroy();
 	NKEngineLogSingleton::destroy();
-	NKLog::ConsoleLoggerSingleton::destroy();
-	NKLog::AsyncLogSingleton::destroy();	
+	NKLog::AsyncLogSingleton::destroy();
 }
